3970685a30fa38f2d6c34fd6577469a2751c5504
sercan-esen <sercan1esen@gmail.com>
2023-02-23 18:30:15 +0300

    Splash and Begin OnBoarding

---
+++ app/src/main/java/com/example/movieapp/presentation/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,28 @@
+package com.example.movieapp.presentation.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentViewPagerBinding
+
+
+class ViewPagerFragment : Fragment() {
+    private lateinit var binding: FragmentViewPagerBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentViewPagerBinding.inflate(inflater,container,false)
+
+        val fragmentList = arrayListOf<Fragment>(
+
+
+        )
+        return binding.root
+    }
+
+}

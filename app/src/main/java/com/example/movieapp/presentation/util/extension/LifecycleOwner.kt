a8b5e010e536fb6fa1f74b50e1a9406c1a9986c8
sercan-esen <sercan1esen@gmail.com>
2023-02-27 17:02:07 +0300

    add-Base Classes

---
+++ app/src/main/java/com/example/movieapp/presentation/util/extension/LifecycleOwner.kt
@@ -0,0 +1,9 @@
+package com.example.movieapp.presentation.util.extension
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
+fun <T> LifecycleOwner.observe(liveData: MutableLiveData<T>?, observer: (T) -> Unit) {
+    liveData?.observe(this, Observer(observer))
+}
